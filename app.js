400383ef6468359b2bf20e41bb02508c569f1ebe
srenV <225722508+voigtsren-source@users.noreply.github.com>
2025-12-04 13:56:44 +0100

    mobile layout almost done

--- app.js
+++ app.js
@@ -1,49 +1,87 @@
 //Navigation constants
-const sections = document.querySelectorAll('.section')
-const liAnchor = document.querySelectorAll('.liAnchor')
+const sections = document.querySelectorAll(".section");
+const liAnchor = document.querySelectorAll(".liAnchor");
+
+//Mobile navigation
+const mobileMenuBtn = document.querySelector(".mobileMenuBtn");
+const mobileNav = document.getElementById("mobileNav");
+
+const mobileliAnchor = document.querySelectorAll('mobileliAnchor')
 
 //QuackToggle constants
-const quack = new Audio('./assets/075176_duck-quack-40345.mp3')
-const quackBox = document.getElementById('quackBox')
+const quack = new Audio("./assets/075176_duck-quack-40345.mp3");
+const quackBox = document.getElementById("quackBox");
 
 //checks if the current section is in the viewport and adds the "active" class to the according anchor tag in the navigation bar
 function highlightSections() {
-    sections.forEach((section, index) =>{
-        const rect = section.getBoundingClientRect();
+  sections.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
 
-        //checks if the middleline of the viewport is in the section (window.innerheight * 0.5 is the middle of the viewport)
-          if(rect.top < window.innerHeight * 0.5 && rect.bottom > window.innerHeight * 0.5) {
-            //then it adds/removes the active class
-            liAnchor.forEach(li => li.classList.remove('active'))
-            liAnchor[index].classList.add('active')  
-            sections.forEach(sec => sec.classList.remove('active'))
-            sections[index].classList.add('active')
+    //checks if the middleline of the viewport is in the section (window.innerheight * 0.5 is the middle of the viewport)
+    if (
+      rect.top < window.innerHeight * 0.5 &&
+      rect.bottom > window.innerHeight * 0.5
+    ) {
+      //then it adds/removes the active class
+      liAnchor.forEach((li) => li.classList.remove("active"));
+      liAnchor[index].classList.add("active");
+      sections.forEach((sec) => sec.classList.remove("active"));
+      sections[index].classList.add("active");
     }
-    })
+  });
 }
 
 //This eventlistener reacts to scrolling and calls the highlightSection function
-window.addEventListener('scroll', highlightSections)
+window.addEventListener("scroll", highlightSections);
 
-highlightSections()
+highlightSections();
 
 //initialy the check needs to be false
-let quackCheck = false
+let quackCheck = false;
 
-//adds/removes an eventlistener to the whole doument if the checkbox is toggled 
-quackBox.addEventListener('change', (e) =>{
-    quackCheck = e.target.checked
+//adds/removes an eventlistener to the whole doument if the checkbox is toggled
+quackBox.addEventListener("change", (e) => {
+  quackCheck = e.target.checked;
 
-    if(quackCheck){
-        document.addEventListener('click', quackCall)
-    }else{
-        document.removeEventListener('click', quackCall)
-    }
-})
+  if (quackCheck) {
+    document.addEventListener("click", quackCall);
+  } else {
+    document.removeEventListener("click", quackCall);
+  }
+});
 
 //just a helper for the eventlistener to call the sound
-function quackCall(){
-    quack.currentTime = 0.3;
-    quack.play();
+function quackCall() {
+  quack.currentTime = 0.3;
+  quack.play();
 }
 
+//Mobile Hamburger Button and eventlisteners for the closing trough an click outside 
+
+mobileMenuBtn.addEventListener("click", (e) => {
+  if (!mobileNav.classList.contains("active")) {
+    mobileNav.classList.add("active");
+  } else {
+    mobileNav.classList.remove("active");
+  }
+  e.stopPropagation();
+});
+
+mobileNav.addEventListener('click', (e) =>{
+    if(e.target = mobileliAnchor){
+        mobileNav.classList.remove("active");
+    }
+    e.stopPropagation()
+})
+
+document.addEventListener("click", (e) => {
+  mobileNav.classList.remove("active");
+});
+
+// close the mobile nav if a anchor is clicked
+
+mobileliAnchor.forEach((anchor)=>{
+    anchor.addEventListener('click', () =>{
+        mobileNav.classList.remove("active");
+    })
+})
